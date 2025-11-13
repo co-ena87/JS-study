2d5f635435aca96b7cb154246220468eeb66a47b
co-ena87 <co.ena87@gmail.com>
2025-11-13 18:14:27 +0900

    API 가 무엇인지 학습하였다.

---
+++ api.js
@@ -0,0 +1,13 @@
+// let response = fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err));
+
+// console.log(response);
+
+const getData = async () => {
+  let response = await fetch("https://jsonplaceholder.typicode.com/users");
+  let data = await response.json();
+  console.log(data);
+};
+
+getData();
